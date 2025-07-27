73778a48d3a5a48c578990b9a3a48bdd7ab5e62d
ShukriSenawi <shukrisenawi@gmail.com>
2025-07-27 16:39:41 +0800

    kemaskini input selectbox

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -37,33 +37,38 @@
             <x-slot:label>5. Tempat Lahir</x-slot:label>
         </x-input-text>
 
-        <x-radio-button :data="['Lelaki', 'Perempuan']" :value="old('gender')">
+        <x-radio-button :data="['Lelaki' => 'Lelaki', 'Perempuan' => 'Perempuan']" :value="old('gender')">
             <x-slot:id>gender</x-slot:id>
             <x-slot:label>6. Jantina</x-slot:label>
         </x-radio-button>
 
-        <x-dropdown :data="['Melayu', 'Cina', 'India', 'Lain-lain']" :value="old('ethnicity')">
+        <x-dropdown :data="['Melayu' => 'Melayu', 'Cina' => 'Cina', 'India' => 'India', 'Lain-lain' => 'Lain-lain']" :value="old('ethnicity')">
             <x-slot:id>ethnicity</x-slot:id>
             <x-slot:label>7. Etnik</x-slot:label>
         </x-dropdown>
 
-        <x-dropdown :data="['Tidak pernah berkahwin', 'Berhijrah', 'Balu', 'Bercerai']" :value="old('marital_status')">
+        <x-dropdown :data="[
+            'Tidak pernah berkahwin' => 'Tidak pernah berkahwin',
+            'Berhijrah' => 'Berhijrah',
+            'Balu' => 'Balu',
+            'Bercerai' => 'Bercerai',
+        ]" :value="old('marital_status')">
             <x-slot:id>marital_status</x-slot:id>
             <x-slot:label>8. Status Perkahwinan</x-slot:label>
         </x-dropdown>
 
         <x-dropdown :data="[
-            'Tiada pendidikan formal',
-            'Sijil sekolah rendah',
-            'SRP/PMR/LCE',
-            'SPM/SPMV/MCE',
-            'STP/STPM/STAM/HSC',
-            'Sijil kemahiran',
-            'Diploma',
-            'Ijazah Sarjana Muda',
-            'Master',
-            'PhD',
-            'Lain-lain',
+            'Tiada pendidikan formal' => 'Tiada pendidikan formal',
+            'Sijil sekolah rendah' => 'Sijil sekolah rendah',
+            'SRP/PMR/LCE' => 'SRP/PMR/LCE',
+            'SPM/SPMV/MCE' => 'SPM/SPMV/MCE',
+            'STP/STPM/STAM/HSC' => 'STP/STPM/STAM/HSC',
+            'Sijil kemahiran' => 'Sijil kemahiran',
+            'Diploma' => 'Diploma',
+            'Ijazah Sarjana Muda' => 'Ijazah Sarjana Muda',
+            'Master' => 'Master',
+            'PhD' => 'PhD',
+            'Lain-lain' => 'Lain-lain',
         ]" :value="old('education_level')">
             <x-slot:id>education_level</x-slot:id>
             <x-slot:label>10. Tahap Pendidikan Tertinggi</x-slot:label>
@@ -90,50 +95,55 @@
         </x-input-text>
 
         <x-dropdown :data="[
-            'JUSA A',
-            'JUSA B',
-            'JUSA C',
-            'KB 54/KB 14',
-            'KB 52/KB 13',
-            'KB 48/KB 12',
-            'KB 44/KB 10',
-            'KB 41/KB 9',
-            'KB 40/KB 8',
-            'KB 38/KB 7',
-            'KB 32/KB 6',
-            'KB 29/KB 5',
-            'KB 28/KB 4',
-            'KB 26/KB 3',
-            'KB 24/KB 2',
-            'KB 22/KB 1',
-            'KB 19/KB 1',
+            'JUSA A' => 'JUSA A',
+            'JUSA B' => 'JUSA B',
+            'JUSA C' => 'JUSA C',
+            'KB 54/KB 14' => 'KB 54/KB 14',
+            'KB 52/KB 13' => 'KB 52/KB 13',
+            'KB 48/KB 12' => 'KB 48/KB 12',
+            'KB 44/KB 10' => 'KB 44/KB 10',
+            'KB 41/KB 9' => 'KB 41/KB 9',
+            'KB 40/KB 8' => 'KB 40/KB 8',
+            'KB 38/KB 7' => 'KB 38/KB 7',
+            'KB 32/KB 6' => 'KB 32/KB 6',
+            'KB 29/KB 5' => 'KB 29/KB 5',
+            'KB 28/KB 4' => 'KB 28/KB 4',
+            'KB 26/KB 3' => 'KB 26/KB 3',
+            'KB 24/KB 2' => 'KB 24/KB 2',
+            'KB 22/KB 1' => 'KB 22/KB 1',
+            'KB 19/KB 1' => 'KB 19/KB 1',
         ]" :value="old('grade')">
             <x-slot:id>grade</x-slot:id>
             <x-slot:label>Gred</x-slot:label>
         </x-dropdown>
 
-        <x-dropdown value="{{ old('location') }}" :data="['Ibu Pejabat Putrajaya', 'Ibu Pejabat Negeri', 'Zon daerah', 'Balai bomba']">
+        <x-dropdown value="{{ old('location') }}" :data="[
+            'Ibu Pejabat Putrajaya' => 'Ibu Pejabat Putrajaya',
+            'Ibu Pejabat Negeri' => 'Ibu Pejabat Negeri',
+            'Zon daerah' => 'Zon daerah',
+            'Balai bomba' => 'Balai bomba',
+        ]">
             <x-slot:id>location</x-slot:id>
             <x-slot:label>Lokasi</x-slot:label>
         </x-dropdown>
 
         <x-dropdown :value="old('position')" :data="[
-            'Operasi kebombaan dan penyelamat',
-            'Keselamatan kebakaran',
-            'Latihan',
-            'Penyiasatan kebakaran',
-            'Udara',
-            'Pembangunan, kejuruteraan dan logistik',
-            'Perancangan dan penyelidikan',
-            'Pengurusan korporat',
-            'Pengurusan',
-            'Integriti',
+            'Operasi kebombaan dan penyelamat' => 'Operasi kebombaan dan penyelamat',
+            'Keselamatan kebakaran' => 'Keselamatan kebakaran',
+            'Latihan' => 'Latihan',
+            'Penyiasatan kebakaran' => 'Penyiasatan kebakaran',
+            'Udara' => 'Udara',
+            'Pembangunan, kejuruteraan dan logistik' => 'Pembangunan, kejuruteraan dan logistik',
+            'Perancangan dan penyelidikan' => 'Perancangan dan penyelidikan',
+            'Pengurusan korporat' => 'Pengurusan korporat',
+            'Pengurusan' => 'Pengurusan',
+            'Integriti' => 'Integriti',
         ]">
             <x-slot:id>position</x-slot:id>
             <x-slot:label>Bahagian</x-slot:label>
         </x-dropdown>
 
-        <x-dropdown :value="old('state')" :data="['Selangor', 'Kuala Lumpur', 'Putrajaya']">
+        <x-dropdown :value="old('state')" :data="['Selangor' => 'Selangor', 'Kuala Lumpur' => 'Kuala Lumpur', 'Putrajaya' => 'Putrajaya']">
             <x-slot:id>state</x-slot:id>
             <x-slot:label>Negeri</x-slot:label>
         </x-dropdown>
@@ -143,7 +153,10 @@
             <x-slot:label>Tempoh Berkhidmat</x-slot:label>
         </x-input-text>
 
-        <x-radio-button :value="old('service_status')" :data="['Pegawai Sepenuh Masa', 'Pegawai Bomba Bantuan']">
+        <x-radio-button :value="old('service_status')" :data="[
+            'Pegawai Sepenuh Masa' => 'Pegawai Sepenuh Masa',
+            'Pegawai Bomba Bantuan' => 'Pegawai Bomba Bantuan',
+        ]">
             <x-slot:id>service_status</x-slot:id>
             <x-slot:label>Status Perkhidmatan</x-slot:label>
         </x-radio-button>
